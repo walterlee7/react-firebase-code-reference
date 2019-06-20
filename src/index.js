e6f16f4dafb1d6174088f9f7d956edc3c9e9af7f
walterlee <walter.lee9@gmail.com>
2019-06-20 18:42:06 -0500

    react context api setup

--- src/index.js
+++ src/index.js
@@ -3,7 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import Firebase, { FirebaseContext } from './components/Firebase';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <FirebaseContext.Provider value={new Firebase()}>
+        <App />
+    </FirebaseContext.Provider>, document.getElementById('root'));
 
 serviceWorker.unregister();
