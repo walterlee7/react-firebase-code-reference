31ab2884b3012c61949c87fe10c03d56e3c69c89
walterlee <walter.lee9@gmail.com>
2019-06-20 18:21:39 -0500

    firebase config update

--- src/components/Firebase/index.js
+++ src/components/Firebase/index.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import app from 'firebase/app';
 
-const App = () => (
-    <div>
-        <h1>App</h1>
-    </div>
-);
+const config = {
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appID: process.env.REACT_APP_APP_ID,
+};
 
-export default App;
+class Firebase {
+    constructor() {
+        app.initializeApp(config);
+    }
+}
+
+export default Firebase;
